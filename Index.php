c6c3a3802fd68a79279db5772938f1b6c4591f90
abdullahi salihu <abdullahisalihu669@gmail.com>
2025-05-16 18:45:13 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,25 @@
+<!-- save this file as index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page title</title>
+</head>
+<body>
+   <!-- the form -->
+   <form  action="register.php" method="POST">
+   label co
+     Full Name:<input type="text" name="name" required>
+     <br>
+     Address:<input type="text" name="address"required>
+     <br>
+      phone number  :<input type="number" name="number" required>
+      <br>
+      complaint-form <input type="text" name="apeal"required >
+      <br>
+      <button name="submit" >submit</button>   <button type="reset">Reset</button> 
+       
+   </form>
+   <!-- end form -->
+</body>
+</html>
